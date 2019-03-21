368035f61de8ecd682e5a23cb82bbb05dfa2ace7
Ernest <ernest.hayford@bluecrest.edu.gh>
2019-03-21 12:39:49 +0000

    reseted creds

--- api/modules/aws.module.js
+++ api/modules/aws.module.js
@@ -1,9 +1,5 @@
 // Configure and export Amazon Web Services SDK
 const aws = require('aws-sdk');
-aws.config.update({
-  accessKeyId: 'AKIAI3VGVFNCPWJDUV3A',
-  secretAccessKey: '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',
-  region: 'us-east-1'
-})
+aws.config.update(require('../settings/aws'));
 
 module.exports = aws
--- api/settings/email.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  host: 'mail.oneinvader.com',
-  port: 587,
-  secure: false,
-  auth:{
-    user: 'AKIAJFXBKPP7KPQXRAJA',
-    pass: 'BDdmeU8Pz+YzPeqR2YpzgkA+bgu79kF36rkbzLHYt8Rt'
-  }
-};
